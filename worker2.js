f6cb4d6e113736f1c4dd63514e1fc4e6b2746a11
anhhoang236 <hoangtran2306@gmail.com>
2017-06-23 11:01:02 +0700

    fix test

--- worker2.js
+++ worker2.js
@@ -7,7 +7,8 @@ var path = require('path');
 const worker = {};
 
 worker.init = function () {
-  worker.LOCK = false;
+  worker.QUEUE = [];
+  worker.FLAG = 3;
   worker.LOCATION_EXE = path.join(__dirname, 'execution/location_ps/templater.ps1 -m');
   worker.INTRO_EXE = path.join(__dirname, 'execution/intro_ps/templater.ps1 -m');
   worker.COMBINED_EXE = path.join(__dirname, 'execution/combined_ps/templater.ps1 -m');
@@ -47,10 +48,32 @@ worker.init = function () {
     obj3.source_footage = path.join(__dirname, 'output/Output');
     obj3.output_location = path.join(__dirname, 'output/Output');
     fs.writeFileSync(this.COMBINED_OPTS, JSON.stringify(obj3), 'utf8');
+
+    worker.start();
   } catch (e){
     console.log('Init worker error!');
     console.log(e);
   }
+};
+
+worker.start = function () {
+  if (this.QUEUE.length > 0 && this.FLAG > 0){
+    job = this.QUEUE.shift();
+    this.FLAG--;
+    this.process(job);
+  }
+  setTimeout(this.start, 30000);
+};
+
+worker.process = function (job) {
+  cmd = this.INTRO_EXE;
+  if (job.type === 'location') cmd = this.LOCATION_EXE;
+  fs.writeFileSync(this.INTRO_DATA, JSON.stringify(job.data), 'utf8');
+  child = spawn('powershell.exe',[cmd]);
+  child.on("exit",function(){
+    this.FLAG++;
+    console.log(job.type + " has been rendered!");
+  });
 };
 
 worker.solve = function (tripData) {
@@ -61,14 +84,10 @@ worker.solve = function (tripData) {
     image: tripData.intro,
     time: tripData.time
   }];
-  fs.writeFileSync(this.INTRO_DATA, JSON.stringify(intro), 'utf8');
-  child = spawn('powershell.exe',[this.INTRO_EXE]);
-  child.on("exit",function(){
-    console.log("Intro has been added to queue!");
-  });
+  worker.QUEUE.push({ type: 'intro', data: intro });
   var i = 1;
-  var locations = tripData.locations.map(function(location) {
-    return {
+  tripData.locations.forEach(function(location) {
+    worker.QUEUE.push({ type: 'location', data: [{
       output: tripData.id + '_' + i++,
       name: location.name,
       temperature: location.temperature,
@@ -78,28 +97,24 @@ worker.solve = function (tripData) {
       image3: location.image3,
       image4: location.image4,
       image5: location.image5
-    };
+    }] });
   });
-  fs.writeFileSync(this.LOCATION_DATA, JSON.stringify(locations), 'utf8');
-  child2 = spawn('powershell.exe',[this.LOCATION_EXE]);
-  child2.on("exit",function(){
-    console.log("Locations have been added to queue!");
-  });
+
   var combined = [{
     output: tripData.id,
-    intro: 'intro_' + tripData.id + '.mp4'
+    intro: 'intro_' + tripData.id + '.mov'
   }];
-  if (locations[0]) combined[0].location1 = 'location_' + locations[0].output + '.mp4';
-  if (locations[1]) combined[0].location2 = 'location_' + locations[1].output + '.mp4';
-  if (locations[2]) combined[0].location3 = 'location_' + locations[2].output + '.mp4';
-  if (locations[3]) combined[0].location4 = 'location_' + locations[3].output + '.mp4';
-  if (locations[4]) combined[0].location5 = 'location_' + locations[4].output + '.mp4';
-  if (locations[5]) combined[0].location6 = 'location_' + locations[5].output + '.mp4';
-  if (locations[6]) combined[0].location7 = 'location_' + locations[6].output + '.mp4';
-  if (locations[7]) combined[0].location8 = 'location_' + locations[7].output + '.mp4';
-  if (locations[8]) combined[0].location9 = 'location_' + locations[8].output + '.mp4';
-  if (locations[9]) combined[0].location10 = 'location_' + locations[9].output + '.mp4';
-  if (locations[10]) combined[0].location11 = 'location_' + locations[10].output + '.mp4';
+  if (locations[0]) combined[0].location1 = 'location_' + locations[0].output + '.mov';
+  if (locations[1]) combined[0].location2 = 'location_' + locations[1].output + '.mov';
+  if (locations[2]) combined[0].location3 = 'location_' + locations[2].output + '.mov';
+  if (locations[3]) combined[0].location4 = 'location_' + locations[3].output + '.mov';
+  if (locations[4]) combined[0].location5 = 'location_' + locations[4].output + '.mov';
+  if (locations[5]) combined[0].location6 = 'location_' + locations[5].output + '.mov';
+  if (locations[6]) combined[0].location7 = 'location_' + locations[6].output + '.mov';
+  if (locations[7]) combined[0].location8 = 'location_' + locations[7].output + '.mov';
+  if (locations[8]) combined[0].location9 = 'location_' + locations[8].output + '.mov';
+  if (locations[9]) combined[0].location10 = 'location_' + locations[9].output + '.mov';
+  if (locations[10]) combined[0].location11 = 'location_' + locations[10].output + '.mov';
   checker.push(combined);
 };
 
