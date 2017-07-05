e269fb928857f4f477a76b4ba0dd123157b08d38
anhhoang236 <hoangtran2306@gmail.com>
2017-07-05 11:24:55 +0700

    fix connect server

--- socket.js
+++ socket.js
@@ -2,48 +2,33 @@ var net = require('net');
 var worker = require('./worker2');
 var checker = require('./checker');
 
-var HOST = '45.32.216.6';
+var HOST = '45.32.93.22';
 var PORT = 6969;
 
-// Create a server instance, and chain the listen function to it
-// The function passed to net.createServer() becomes the event handler for the 'connection' event
-// The sock object the callback function receives UNIQUE for each connection
-net.createServer(function(sock) {
-
-  // We have a connection - a socket object is assigned to the connection automatically
-  console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);
-  // sock.setEncoding('utf8');
-  // Add a 'data' event handler to this instance of socket
-  checker.sock = sock;
+try {
+  const client = new net.Socket();
   var data = '';
-  sock.on('data', function(chunk) {
+  client.connect(PORT, HOST, () => {
+    console.log('Connected');
+	checker.sock = client;
+  });
+  client.on('error', (err) => {
+    console.log(err);
+  });
+  client.on('data', (chunk) => {
     if (chunk.toString() === 'end'){
       try {
         obj = JSON.parse(data);
         // console.log(obj);
         worker.solve(obj);
+		data = '';
       } catch (e){
-
       }
     }
     data += chunk;
   });
-
-  sock.on('end', function() {
-    // worker.solve(obj);
-  });
-
-  // Add a 'close' event handler to this instance of socket
-  sock.on('close', function(data) {
-    console.log('CLOSED: ' + sock.remoteAddress +' '+ sock.remotePort);
-  });
-
-  sock.on('error', function(err) {
-    console.log('CLOSED: ' + err);
-  });
-
-}).listen(PORT, HOST);
-
-worker.init();
-console.log('Server listening on ' + HOST +':'+ PORT);
-checker.start();
+  worker.init();
+  checker.start();
+} catch (e) {
+  console.log(e);
+}
